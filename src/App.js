901fd86f4e027b2178ef98cdae356e3318cd91be
Andy Aldrich <andrew@selrich.com>
2022-10-04 19:29:15 -0600

    nav added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from "./components/NavBar"
+import About from "./screens/About"
+import Home from './screens/Home'
+import Archive from './screens/Archive'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavBar />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route exact path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/archive' element={<Archive />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+
 export default function NavBar() {
     return (
         <div>
-            <h1>Matter</h1>
-            <button class="rounded-lg bg-black text-white" >Menu</button>
+            <nav class="flex justify-center space-x-4">
+                <a href="/home" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+                <a href="/about" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">About</a>
+                <a href="/archive" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Archive</a>
+            </nav>
         </div>
     )
 }
