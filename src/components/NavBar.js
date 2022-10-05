8bfdef9489626dfe3b46007256c353050852e25f
Sean Fay <saltycrackersagain@gmail.com>
2022-10-05 17:48:38 -0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <div class="bg-black flex justify-between p-2">
             <a href="/home">
-                <Logo class="hover:animate-ping ..." />
+                <Logo class="hover:motion-safe:animate-pulse ..." />
             </a>
             <nav class="flex justify-evenly space-x-4">
                 <a href="/home" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 hover:rounded-sm">Home</a>
