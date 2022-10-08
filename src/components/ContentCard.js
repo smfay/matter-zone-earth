4836877f3a8d160c07b6aa1a0ffa7659451eb7c5
Sean Fay <saltycrackersagain@gmail.com>
2022-10-07 19:26:55 -0500

    did some things

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import image from '../assets/images/default.png'
 export default function ContentCard() {
     return (
-        <div class="bg-black">
+        <div class="bg-black p-2">
+            <img src={image} class="aspect-square" />
+            <h1 class="text-3xl font-black">Excepteur sint occaecat cupidatat non proident</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div>
+
+            </div>
         </div>
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ContentCard from '../components/ContentCard'
+import ContentGrid from '../components/ContentGrid'
 
 export default function Home() {
   return (
     <div>
       Home
-      <ContentCard />
+      <div class="p-10 flex justify-center space-x-10 columns-4">
+        <ContentCard />
+        <ContentCard />
+        <ContentCard />
+        <ContentCard />
+        <ContentCard />
+        <ContentCard />
+      </div>
     </div>
   )
 }
