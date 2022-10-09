7c9f6b4b673de72ba45408482e2a2ae95a5650b4
Sean Fay <saltycrackersagain@gmail.com>
2022-10-09 13:11:02 -0500

    more styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import image from '../assets/images/default.png'
+import Logo from '../assets/svg/Logo'
 
 export default function Banner() {
     return (
-        <span class="overflow-hidden h-40 w-full">
-            <img src={image} class="w-screen">
-            </img>
-        </span>
+        <div class="flex bg-right p-0 overflow-hidden w-full bg-main bg-fixed">
+            <span class=" p-32 w-screen h-full">
+            </span>
+        </div>
     )
 }
--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -2,18 +2,18 @@ import React from 'react'
 import image from '../assets/images/default.png'
 export default function ContentCard() {
     return (
-        <div class="min-h-fit flex flex-row hover:shadow-inner shadow-white hover:skew-y-3 hover:-skew-x-3 m-5 sm:w-full md:w-1/4 lg:w-1/4 border-2 hover:border-4 border-white hover:p-2 aspect-video bg-black p-8 rounded-lg space-x-1 space-y-3 transition-all ease-in-out duration-300">
-            <div class="w-full float-left bg-fixed overflow-hidden aspect-square rounded-lg m-2">
-                <img src={image} class="w-full" />
+        <div class="flex flex-col items-center min-h-fit border-2 border-white hover:skew-y-3 hover:-skew-x-3 m-5 sm:w-full md:w-3/4 lg:w-1/5 hover:p-2 bg-black p-4 rounded-lg transition-all ease-in-out duration-75">
+            <div class="overflow-hidden rounded-lg">
+                <img src={image} class=".object-cover" />
             </div>
-            <span class="flex flex-col m-10">
-                <h1 class="text-2xl font-bold">Excepteur sint occaecat cupidatat non proident</h1>
+            <span class="justify-center items-center p-4">
+                <h1 class="text-3xl font-bold py-5">Excepteur sint occaecat cupidatat non proident</h1>
                 <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <div class="flex justify-end">
-                    <a>Category</a>
-                    <a>Author</a>
+                <div class="flex space-x-5 justify-end items-center">
+                    <p class="border-white rounded-full border-2 py-1 px-3 m-3">Author</p>
+                    <p class="border-white rounded-full border-2 py-1 px-3 m-3">Category</p>
                 </div>
             </span>
-        </div>
+        </div >
     )
 }
