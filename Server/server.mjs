d2c326eded8028a9df4a3eeae2514b0c36c8b23e
Sean Fay <saltycrackersagain@gmail.com>
2023-02-04 16:12:24 -0600

    Express server

---
+++ Server/server.mjs
@@ -0,0 +1,13 @@
+import sanity from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+import { searchQuery } from './FeedData.mjs'
+
+let client = sanity({
+    projectId: "x7moib4u",
+    dataset: "production",
+    useCdn: true,
+    apiVersion: "2022-10-12",
+})
+
+client.fetch(searchQuery("Denote"))
+    .then(data => console.log(data))
